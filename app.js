2ef79c821992cfb58bc9ab608ec54a6295265b75
hassanceltic <108281622+hassanceltic@users.noreply.github.com>
2024-03-06 14:45:45 +0100

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Hassan !!'));
+app.get('/', (req, res) => res.send('Test manual approvall is successfull'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
